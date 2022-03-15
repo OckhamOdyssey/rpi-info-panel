d05eae5f154c05f2927687569e48a30809cd6174
OckhamOdyssey <sariego.nicolas@gmail.com>
2022-03-15 17:09:18 +0100

    changed JS variables for clock

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function doDate()
 {
     date = new Date()
-    document.getElementById('local-time').innerHTML = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    myHour = '0' + date.getHours();
+    myMinute = '0' + date.getMinutes();
+    mySeconds = '0' + date.getSeconds();
+    document.getElementById('local-time').innerHTML = myHour.slice(-2) + ':' + myMinute.slice(-2) + ':' + mySeconds.slice(-2);
 }
 setInterval(doDate, 1000);
